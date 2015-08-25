a31424569c24a427ceb69d006b0a77607cad52e3
legoboy0215 <legoboy0215weebly@gmail.com>
2015-08-25 14:23:10 +0800

    Update Main.php

--- src/Legoboy/WorldStats/Main.php
+++ src/Legoboy/WorldStats/Main.php
@@ -1,1 +1,17 @@
 <?php
+namespace Legoboy\WorldStats;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\SignChangeEvent;
+
+use pocketmine\tile\Tile;
+use pocketmine\tile\Sign;
+
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+}
